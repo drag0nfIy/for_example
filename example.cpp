f8aac27d3a7c0a0e2d4e0ae2142bb6259404ef49
Даша <pineapple-for-hedgehog@mail.ru>
2022-08-09 13:48:30 +0300

    first commit

---
+++ example.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main (){
+	int a, b;
+	cout << "hello world" << endl;
+	cin >> a >> b;
+	cout << "a + b = " << a+b << endl;
+	for (int i = 0; i < a+b; i++) cout << "hello, world" << " ";
+}
