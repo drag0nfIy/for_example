1879429fc6a1367d2c0555d01ef5cdfe503a4cb3
hedgehog <pineapple-for-hedgehog@mail.ru>
2022-08-15 13:43:23 +0300

    Pawn move

--- main.cpp
+++ main.cpp
@@ -3,29 +3,92 @@
 #include <iomanip>
 #include <cstdlib>
 #include <algorithm>
+#include <windows.h>
 
 using namespace std;
+
     char A[9][9]{
-    {'8','r','b','k','e','q','k','b','r'},
+    {'8','r','h','b','q','k','b','h','r'},
     {'7','p','p','p','p','p','p','p','p'},
     {'6','0','0','0','0','0','0','0','0'},
     {'5','0','0','0','0','0','0','0','0'},
     {'4','0','0','0','0','0','0','0','0'},
     {'3','0','0','0','0','0','0','0','0'},
     {'2','P','P','P','P','P','P','P','P'},
-    {'1','R','B','K','E','Q','K','B','R'},
-    {' ', 'A','B','C','D','E','F','G','H'}
+    {'1','R','H','B','Q','K','B','H','R'},
+    {' ','A','B','C','D','E','F','G','H'}
 	};
+	string up = "PRHBQK"; // для проверки на взятие своих фигур
+	string down = "prhbqk";
+	
+	int determine = 0; //счётчик для определения противника.Б.Ф(Верхний регистр)-нечётные; ч.ф(нижний р)-чётные.
+	struct chess_figures{
+		bool define_figure(int i, int j, int new_i, int new_j){
+			bool flag = false;
+			if (A[i][j] == 'P' || A[i][j] == 'p') { return flag = pawn_move(i, j, new_i, new_j); }
+			if (A[i][j] == 'R' || A[i][j] == 'r') { return flag = rook_move(i, j, new_i, new_j); }
+			if (A[i][j] == 'B' || A[i][j] == 'b') { return flag = bishop_move(i, j, new_i, new_j); }
+			if (A[i][j] == 'H' || A[i][j] == 'h') { return flag = horse_move(i, j, new_i, new_j); }
+			if (A[i][j] == 'K' || A[i][j] == 'k') { return flag = king_move(i, j, new_i, new_j); }
+			if (A[i][j] == 'Q' || A[i][j] == 'q') { return flag = qween_move(i, j, new_i, new_j); }
+			return flag; // возвращение флага
+		}
+		bool pawn_move(int i, int j, int new_i, int new_j){ //простой ход без взятия
+		if (!(determine % 2)){// black
+			if (j == new_j){
+				if (new_i == i+1) return true;
+				if ((new_i == i + 2 && i == 1)&& A[i+1][j] == '0') return true;
+				
+			}
+			else if (new_i == i + 1 && A[new_i][new_j] != '0'){
+				if (new_j == j + 1 || new_j == j - 1){
+					for (int k = 0; k < up.size(); k++){
+						if (A[new_i][new_j] == up[k]) return true;
+					}
+				}
+				else return false;
+			}
+		}
+		if (determine % 2){ // WHITE
+			if (j == new_j){
+				if (new_i == i-1 ) return true;
+				if ((new_i == i - 2 && i == 6) && A[i-1][j] == '0') return true;
+			}
+			else if (new_i == i - 1 && A[new_i][new_j] != '0'){
+				if (new_j == j + 1 || new_j == j - 1){
+					for (int k = 0; k < down.size(); k++){
+						if (A[new_i][new_j] == down[k]) return true;
+					}
+				}
+				else return false;
+			}
+		}
+		return false;
+		}
+
+		bool rook_move(int i, int j, int new_i, int new_j) {}
+		bool bishop_move(int i, int j, int new_i, int new_j) {}
+		bool horse_move(int i, int j, int new_i, int new_j) {}
+		bool king_move(int i, int j, int new_i, int new_j) {}
+		bool qween_move(int i, int j, int new_i, int new_j) {}
+	};
+
 	void print(){
+		cout << "\n     A B C D E F G H" << endl;
 		for (int i = 0; i < 9; i++) {
+			cout << "  ";
         	for (int j = 0; j < 9; j++) cout << setw(2) << A[i][j];
-        cout << endl;
-    	}	
+			if (i != 8) cout << " " << 8 - i;
+     		cout << endl;
+		}
+		cout << endl;
 	}
-	void input(){
-		int b; char a;
-		cin >> a >> b;
+	
+	void rearranging(int i, int j, int new_i, int new_j){
+		A[new_i][new_j] = A[i][j];
+		A[i][j] = '0';
 	}
+
 	int switch_char (char ch, bool &flag){ //j (столбец)
         int j;
 		switch (ch){
@@ -69,38 +132,39 @@ using namespace std;
 		}
         return j;
 	}
-	int switch_int (int b, bool &flag){//i (строка)
+
+	int switch_int (char int_, bool &flag){//i (строка)
         int i;
-		switch (b){
-			case 1:{
+		switch (int_){
+			case '1':{
 				i = 7;
 				break;
 			}
-			case 2:{
+			case '2':{
 				i = 6;
 				break;
 			}
-			case 3:{
+			case '3':{
 				i = 5;
 				break;
 			}
-			case 4:{
+			case '4':{
 				i = 4;
 				break;
 			}
-			case 5:{
+			case '5':{
 				i = 3;
 				break;
 			}
-			case 6:{
+			case '6':{
 				i = 2;
 				break;
 			}
-			case 7:{
+			case '7':{
 				i = 1;
 				break;
 			}
-			case 8:{
+			case '8':{
 				i = 0;
 				break;
 			}
@@ -113,30 +177,53 @@ using namespace std;
         return i;
 	}
 
+	
+
 int main(){
 	print(); // вывод матрицы на консоль
-	//int i, j;  индексы матрицы. i - строка(cooтветствует цифрам); j - столбец(соотв. буквам)
-	char ch, new_ch; int a, new_a, separator; // номер клетки
-    int i, j, new_i, new_j;
-	bool flag = 1; 
-
+	//determine = 0 - счётчик для определения противника. Б.Ф(Верхний регистр)-нечётные; ч.ф(нижний р)-чётные
+	char ch, new_ch, a, new_a; // номер клетки
+    int i, j, new_i, new_j;	//индексы матрицы. i - строка(cooтветствует цифрам); j - столбец(соотв. буквам). new_ - индекс перемещения
+	bool flag = 1, for_wrong_move = 1; 
+	chess_figures step;
+	while(true){
+		
+		for_wrong_move = 1;
+		determine++;
+		if ((determine % 2)) cout << " white's move: ";
+		else cout << " black's move: ";
 	do{ // текущее местонахождение фигуры
 		flag = 1;
 		cin >> ch >> a;
 		j = switch_char(ch, flag);
 		if (flag) i = switch_int(a, flag);
+		if (flag) {
+			if (A[i][j] == '0'){
+				cout << " error: empty cell.\n Re-entry: ";
+				flag = 0;
+			}
+		}
 	} while (!flag);
-	//cout << i << " " << j << endl;
-    //cin >> separator;
+
     do{ // место, куда нужно переставить фигуру
 		flag = 1;
 		cin >> new_ch >> new_a;
 		new_j = switch_char(new_ch, flag);
 		if (flag) new_i = switch_int(new_a, flag);
 	} while (!flag);
-	//cout << new_i << " " << new_j << endl;
-    swap(A[i][j], A[new_i][new_j]);
-    print();
-    system("pause");
 	
+	if (step.define_figure(i, j, new_i, new_j)){
+	    rearranging (i, j, new_i, new_j); // перемещение фигуры
+	} 
+	else {
+		for_wrong_move = 0;
+		cout << " wrong move" << endl;
+		determine--;
+		continue;
+	}
+	if (for_wrong_move) system("cls");
+	print();
+	}
+
+    system("pause");
 }
